82189a262110a59c687ed74844d14e465f392547
Alyssa Brown <alyssa.brown@live.com>
2019-03-26 01:44:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books ORDER BY year  WHERE ";
+  "SELECT title, year FROM books WHERE ORDER BY year";
   SELECT column_name, column_name
 FROM table_name
 ORDER BY column_name ASC|DESC, column_name ASC|DESC;
